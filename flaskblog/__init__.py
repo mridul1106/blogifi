a0cb688263e7f72a8e23ea1eb7efb04cc2581d1e
Mansi-Shukla <mansishukla2505@gmail.com>
2020-12-16 20:04:34 +0530

    file modify

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -16,8 +16,8 @@ db = SQLAlchemy(app)
 db.init_app(app)
 
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
+'''app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)'''
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
