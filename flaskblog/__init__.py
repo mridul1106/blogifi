91ec8a86359f7a02f4198c4d0f947c9533c0ca76
mridul1106 <mridulshukla1106@gmail.com>
2020-12-17 22:09:26 +0530

    c

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -27,9 +27,6 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'flask.blog11@gmail.com'
 app.config['MAIL_PASSWORD'] = 'blog@1465'
-
-app.config['MAIL_USERNAME'] = flask.blog11@gmail.com
-app.config['MAIL_PASSWORD'] = blog@1465
 mail = Mail(app)
 
 from flaskblog import routes
