7981ba8add8345e7c23900b9d3367923a41e46da
mridul1106 <mridulshukla1106@gmail.com>
2020-12-23 20:54:18 +0530

    smtp

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -25,8 +25,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config['MAIL_USERNAME'] = os.environ('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ('EMAIL_PASS')
 mail = Mail(app)
 
 
