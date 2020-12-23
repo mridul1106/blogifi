87225ff513790bc08bab1aa25b9ecd8cdd57d2b3
mridul1106 <mridulshukla1106@gmail.com>
2020-12-23 23:41:40 +0530

    as

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -23,11 +23,11 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
-app.config['MAIL_SERVER'] = 'smtp.zoho.in'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('BLOGIFI_MAIL')
-app.config['MAIL_PASSWORD'] = os.environ.get('BLOGIFI_PASS')
+app.config['MAIL_USERNAME'] = 'flask.blog11@gmail.com'
+app.config['MAIL_PASSWORD'] = 'blog@1465'
 mail = Mail(app)
 
 
